c0a3ce0615968a73901abc662973bc251cb77ad6
Roman Grinyov <w3lifer@gmail.com>
2023-07-06 22:05:13 +0300

    cosmetic changes

--- .docker/.helpers/change-config.php
+++ .docker/.helpers/change-config.php
@@ -4,14 +4,15 @@ const PATH_TO_DB_CONFIG = 'config/db.php';
 
 $config = file_get_contents(PATH_TO_DB_CONFIG);
 
-$config = preg_replace('/host=localhost/', 'host=mysql', $config);
-$config = preg_replace('/dbname=yii2basic/', 'dbname=' . getenv('MYSQL_DATABASE'), $config);
+$config = preg_replace('=host\=localhost=', 'host=mysql', $config);
+$config = preg_replace('=dbname\=yii2basic=', 'dbname=' . getenv('MYSQL_DATABASE'), $config);
 
-$config = preg_replace("/'username' => 'root'/", "'username' => '" . getenv('MYSQL_USER') . "'", $config);
-$config = preg_replace("/'password' => ''/", "'password' => '" . getenv('MYSQL_PASSWORD') . "'", $config);
+$config = preg_replace("='username' \=> 'root'=", "'username' => '" . getenv('MYSQL_USER') . "'", $config);
+$config = preg_replace("='password' \=> ''=", "'password' => '" . getenv('MYSQL_PASSWORD') . "'", $config);
 
 file_put_contents(PATH_TO_DB_CONFIG, $config);
 
+// ---------------------------------------------------------------------------------------------------------------------
 
 const PATH_TO_WEB_CONFIG = 'config/web.php';
 
